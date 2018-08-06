83c49074b257a112f0fd2e7d5f349ea7ed9b5cbe
MReed41 <41068779+MReed41@users.noreply.github.com>
2018-08-06 13:13:58 -0500

    Add project files.

---
+++ BestBuy Corportate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BestBuy_Corportate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
