ece0923fd7c1291aedecb06a49231b62b4854f08
MReed41 <41068779+MReed41@users.noreply.github.com>
2018-08-22 05:05:46 -0500

    CRUD: need maintenance

--- BestBuy Corportate/Product.cs
+++ BestBuy Corportate/Product.cs
@@ -14,7 +14,23 @@ namespace BestBuy_Corportate
         {
             connectionString = _connectionString;
         }
+        public void Insert (Product product)
+        {
+            MySqlConnection conn = new MySqlConnection(connectionString);
 
+            using (conn)
+            {
+                conn.Open();
+                MySqlConnection cmd = conn.CreateCommand();
+
+                cmd.CommandText = "INSERT INTO products (Name, Price)" +
+                    "Values (@name, @price);";
+                cmd.Parameters.AddWithValue("name", product.Name);
+                cmd.Parameters.AddWithValue("price", product.Price);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
         public List<string> GetMyProductNames()
         {
             MySqlConnection conn = new MySqlConnection(connectionString);
@@ -37,7 +53,40 @@ namespace BestBuy_Corportate
                 }
                 return ProductNames;
             }
+        }
 
+        public void Update(Product product)
+        {
+            MySqlConnection conn = new MySqlConnection(connectionString);
+
+            using (conn)
+            {
+                conn.Open();
+                MySqlConnection cmd = new MySqlConnection(connectionString);
+
+                cmd.CommandText = "UPDATE products " + "SET Name = @name, Price = @price " +
+                                    "WHERE ProductId = @id;";
+                cmd.Parameters.AddWithValue("name", product.Name);
+                cmd.Parameters.AddWithValue("price", product.Price);
+                cmd.Parameters.AddWithValue("id", product.Id);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Delet(Product product)
+        {
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            using (conn)
+            {
+                conn.Open();
+                MySqlCommand cmd = conn.CreateCommand();
+
+                cmd.CommandText = "DELETE FROM products " + "WHERE ProductID = @id;";
+                cmd.Parameters.AddWithValue("id", product.Id);
+
+                cmd.ExecuteNonQuery();
+            }
         }
 
     }
